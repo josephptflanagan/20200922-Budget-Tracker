46fea913e4b6b665d7bbd5a0d7007bb2c1af51d3
Joe Flanagan <josephptflanagan@gmail.com>
2020-09-23 10:21:13 -0500

    offline functionality operational

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -33,10 +33,56 @@ request.onerror = function (event) {
 function saveRecord(record) {
     // open a new transaction with the database with read and write permissions 
     const transaction = db.transaction(['new_transaction'], 'readwrite');
-  
+
     // access the object store for `new_transaction`
     const transactionObjectStore = transaction.objectStore('new_transaction');
-  
+
     // add record to your store with add method
     transactionObjectStore.add(record);
-  }
+}
+
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access your object store
+    const transactionObjectStore = transaction.objectStore('new_transaction');
+
+    // get all records from store and set to a variable
+    const getAll = transactionObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function () {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['new_transaction'], 'readwrite');
+                    // access the new_transaction object store
+                    const transactionObjectStore = transaction.objectStore('new_transaction');
+                    // clear all items in your store
+                    transactionObjectStore.clear();
+
+                    alert('All saved transactions have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
+
+// listen for app coming back online
+window.addEventListener('online', uploadTransaction);
